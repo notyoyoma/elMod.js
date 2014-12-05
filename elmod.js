90497e50bf984535f7c06bc05786ff800724551b
Marty Naselli <marty.naselli@gmail.com>
2014-12-05 16:59:14 -0500

    added vertical-align. Improved calling structure

--- elmod.js
+++ elmod.js
@@ -6,15 +6,19 @@
     COLCOUNT: 12,
     COLCLASS: "col-xs-",
     types: {
-      color: function($this) {
-        var tmpl = $('<input type="text">');
+      color: function($this, property, label) {
+        var tmpl = $('<input type="text" placeholder="'+label+'">');
+        tmpl.on('keypress keyup change blur', function() {
+          $this.css(property, $(this).val());
+          $(this).css('background', $(this).val());
+        });
         return tmpl;
       },
       text: function($this) {
         var tmpl = $('<input type="text">');
         return tmpl;
       },
-      align: function($this) {
+      'text-align': function($this) {
         var tmpl = $('<div>');
         tmpl.append($('<input type="radio" name="text-align" id="left"><label for="left">Left</label>'));
         tmpl.append($('<input type="radio" name="text-align" id="center"><label for="center">Center</label>'));
@@ -23,14 +27,30 @@
           $this.css('text-align', $(this).attr('id'));
         });
         return tmpl;
+      },
+      'vertical-align': function($this) {
+        if ($this.css('display') !== "table") {
+          var content = $this.text();
+          $this.empty();
+          $this.append('<div style="display:table"><div style="display:table-row"><div style="display:table-cell" class="v-align-table-cell">'+content+'</div></div></div>')
+        }
+        var tmpl = $('<div>');
+        tmpl.append($('<input type="radio" name="vertical-align" id="top"><label for="top">Top</label>'));
+        tmpl.append($('<input type="radio" name="vertical-align" id="middle"><label for="middle">Middle</label>'));
+        tmpl.append($('<input type="radio" name="vertical-align" id="bottom"><label for="bottom">Bottom</label>'));
+        tmpl.find('input').change(function() {
+          $this.find('.v-align-table-cell').css('vertical-align', $(this).attr('id'));
+        });
+        return tmpl;
       }
     },
   };
   options.propertyMap = {
-    'background-color':  { label: "Background Color", input: options.types.color },
-    'color':             { label: "Text Color", input: options.types.color },
-    'border-color':      { label: "Border Color", input: options.types.color },
-    'text-align':        { label: "Text Align", input: options.types.align },
+    'background-color':  function($this, property) { return options.types.color($this, property, "Background Color" ); },
+    'color':             function($this, property) { return options.types.color($this, property, "Text Color" ); },
+    'border-color':      function($this, property) { return options.types.color($this, property, "Border Color" ); },
+    'text-align':        function($this, property) { return options.types['text-align']($this, property, "Text Align"); },
+    'vertical-align':        function($this, property) { return options.types['vertical-align']($this, property, "Vertical Align"); },
   };
   
   /* private class */
@@ -55,11 +75,11 @@
       var colClass = options.COLCLASS + Math.floor(options.COLCOUNT / arr.length);
       for (var i = 0; i < arr.length; i += 1) {
         var cur = arr[i],
-            fieldset = $('<fideldset style="position:relative;">').append($('<legend>'+cur.title+'</legend>')),
+            fieldset = $('<fideldset style="position:relative;display:block;">').append($('<legend>'+cur.title+'</legend>')),
             row = $('<div class="row"></div>');
         // Setup expanding functionality
         if (cur.hidden) {
-          fieldset.append('<span class="indicator" style="position:absolute;top:-0.5em;right:1em;">+</span>');
+          fieldset.append('<span class="indicator" style="position:absolute;top:-0;right:1em;">+</span>');
           fieldset.click(function() {
             var $this = $(this),
                 target = $this.find('.row'),
@@ -76,7 +96,7 @@
         }
         for (var j = 0; j < cur.fields.length; j += 1) {
           var input = cur.fields[j];
-          if (typeof input === "string" && options.propertyMap[input]) input = options.propertyMap[input].input($this);
+          if (typeof input === "string" && options.propertyMap[input]) input = options.propertyMap[input]($this, input);
           row.append( $('<div class="'+colClass+'">').append(input) );
         }
         this.form.append(fieldset.append(row));
