13628be330b1465e2fce0b28b8a5ad3d210fe241
Marty Naselli <marty.naselli@gmail.com>
2014-12-18 11:31:26 -0500

    updated demo

--- demo/demo.js
+++ demo/demo.js
@@ -1,10 +1,10 @@
 $('#el').elModInit(
   [
+    { title: "Font", fields: ["font-family", "font-size", 'font-weight', 'font-style']},
     { title: "Align", fields: ["text-align", "vertical-align"]},
     { title: "Colors", fields: ["background-color", "color"]},
+    { title: "Dimensions", fields: ["width", "height", "margin", "padding"]},
     { title: "Advanced", fields: ["transform", "box-shadow", "border-radius"], hidden: true }
   ]
 );
-$('#el').click(function() {
-  $('#form').append($(this).elMod());
-});
+$('#form').append($('#el').elModGet());
