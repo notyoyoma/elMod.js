38303e2c48925a24ba3d1538cd7e254b012c9327
Marty Naselli <marty.naselli@gmail.com>
2014-12-31 11:29:36 -0500

    latest demo changes

--- demo/demo.js
+++ demo/demo.js
@@ -1,11 +1,12 @@
 $('#el').elModInit(
   [
-    { title: "Text", fields: ["text"]},
-    { title: "Font", fields: ["font-family", "font-size", 'font-weight', 'font-style']},
-    { title: "Align", fields: ["text-align", "vertical-align"]},
-    { title: "Colors", fields: ["background-color", "color"]},
-    { title: "Dimensions", fields: ["width", "height", "margin", "padding"]},
-    { title: "Advanced", fields: ["transform", "box-shadow", "border-radius"], hidden: true }
+    { legend: "Text",       fields: ["text", "text-transform", "text-decoration"]},
+    { legend: "Font",       fields: [["font-family", "font-size"], ["font-weight", "font-style"], ["color", "text-shadow"]]},
+    { legend: "Type",       fields: ["letter-spacing", "word-spacing", "line-height", "white-space"]},
+    { legend: "Align",      fields: ["text-align", "vertical-align"]},
+    { legend: "Background", fields: ["background-color", "background-image", "background-repeat"]},
+    { legend: "Dimensions", fields: ["width", "height", "margin", "padding"]},
+    { legend: "Advanced",   fields: ["transform", "box-shadow", "border-radius"], hidden: true }
   ]
 );
 $('#form').append($('#el').elModGet());
