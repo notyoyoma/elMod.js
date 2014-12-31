567a211151c040117b980f29031f9995364c1142
Marty Naselli <marty.naselli@gmail.com>
2014-12-31 11:29:54 -0500

    Major Refactor
    Added class helper
    	Stores misc helper functions
    Added class elBuild
    	Used to build elements
    Added several new CSS attributes

--- elmod.js
+++ elmod.js
@@ -7,40 +7,58 @@
     COLCLASS: "col-xs-",
     FONTS: [ "Arial", "Comic Sans MS", "Courier", "Geneva", "Georgia", "Helvetica", "Impact",
       "Lucida Console", "Lucida Sans Unicode", "Palatino Linotype", "Times New Roman", "Trebuchet MS", "Verdana"],
+    UNITS: [ "px", "%", "em", "rem" ],
   };
   options.types = {
+    '_dimension': function($this, property, opts) {
+      var units = (opts && opts.units) ? opts.units : options.UNITS,
+          tmpl = elBuild.dimension(units);
+      tmpl.find('input').val(parseInt($this.css(property)));
+      tmpl.find('select').val($this.css(property).replace(/\d/g, ''));
+      tmpl.find('input, select').on( 'change keyup keypress', function() {
+        var unit = (units.length > 1) ? tmpl.find('select').val() : units;
+        $this.css(property, tmpl.find('input').val()+unit);
+      });
+      return tmpl;
+    },
     '_select': function($this, property, opts) {
       if (!opts && !opts.options) return 'error: _select needs options.';
-      var tmpl = $('<select>')
-          i = 0;
-      for (i; i < options.FONTS.length; i += 1) {
-        tmpl.append('<option>'+options.FONTS[i]+'</option>');
-      }
-      tmpl.val($this.css(property));
+      var tmpl = elBuild.select(opts.options);
+      tmpl.val($this.css(property).replace(/['"]/g,''));
       tmpl.change(function() {
         $this.css(property, $(this).val());
       });
       return tmpl;
     },
-    'dimension': function($this, property, opts) {
-      var tmpl = $('<div>'+opts.label+':</div>');
-      tmpl.append($('<input type="number">'));
-      tmpl.append($('<select><option>px</option><option>%</option><option>em</option><option>rem</option></select>'));
-      tmpl.find('input').val(parseInt($this.css(property)));
-      tmpl.find('select').val($this.css(property).replace(/\d/g, ''));
-      tmpl.find('input, select').on( 'change keyup keypress', function() {
-        $this.css(property, tmpl.find('input').val()+tmpl.find('select').val());
+    '_radio': function($this, property, opts) {
+      if (!opts && !opts.options) return 'error: _radio needs options.';
+      var tmpl = elBuild.radio(property, opts.options),
+          init_val = tmpl.find('#'+$this.css('text-align'));
+      if (!init_val.length) init_val = tmpl.find('input').first();
+      init_val.attr('checked', true);
+      tmpl.find('input').change(function() {
+        $this.css('text-align', $(this).attr('id'));
+      });
+      return tmpl;
+    },
+    '_url': function($this, property, opts) {
+      var tmpl = $('<input type="text">');
+      tmpl.change(function() {
+        $this.css(property, 'url('+$(this).val()+')');
       });
       return tmpl;
     },
     'color': function($this, property, opts) {
-      var tmpl = $('<input type="text" placeholder="'+opts.label+'">');
+      var tmpl = $('<input type="text">');
       tmpl.val($this.css(property));
-      tmpl.css('background', tmpl.val());
-      tmpl.on('keypress keyup change blur', function() {
+      tmpl.on('keyup change blur', function() {
         $this.css(property, $(this).val());
-        $(this).css('background', $(this).val());
+        $(this).css({
+          'background': $(this).val(),
+          'color': helpers.invertColor($(this).val())
+        });
       });
+      tmpl.trigger('change');
       return tmpl;
     },
     'text': function($this) {
@@ -51,31 +69,15 @@
       });
       return tmpl;
     },
-    'text-align': function($this) {
-      var tmpl = $('<div>');
-      tmpl.append($('<input type="radio" name="text-align" id="left"><label for="left">Left</label>'));
-      tmpl.append($('<input type="radio" name="text-align" id="center"><label for="center">Center</label>'));
-      tmpl.append($('<input type="radio" name="text-align" id="right"><label for="right">Right</label>'));
-      var init_val = tmpl.find('#'+$this.css('text-align'));
-      if (!init_val.length) init_val = tmpl.find('input').first();
-      init_val.attr('checked', true);
-      tmpl.find('input').change(function() {
-        $this.css('text-align', $(this).attr('id'));
-      });
-      return tmpl;
-    },
-    'vertical-align': function($this) {
+    'vertical-align': function($this, property, opts) {
       if ($this.css('display') !== "table") {
         var content = $this.text();
         $this.empty();
         $this.css('display', 'table');
         $this.append('<div style="display:table-row"><div style="display:table-cell" class="v-align-table-cell">'+content+'</div></div>')
       }
-      var tmpl = $('<div>');
-      tmpl.append($('<input type="radio" name="vertical-align" id="top"><label for="top">Top</label>'));
-      tmpl.append($('<input type="radio" name="vertical-align" id="middle"><label for="middle">Middle</label>'));
-      tmpl.append($('<input type="radio" name="vertical-align" id="bottom"><label for="bottom">Bottom</label>'));
-      var init_val = tmpl.find('#'+$this.css('text-align'));
+      var tmpl = elBuild.radio(property, ['top', 'middle', 'bottom']),
+          init_val = tmpl.find('#'+$this.css('text-align'));
       if (!init_val.length) init_val = tmpl.find('input').first();
       init_val.attr('checked', true);
       tmpl.find('input').change(function() {
@@ -89,7 +91,7 @@
         text-align,font-size,font-family,font-weight,font-style,text-transform,
         text-decoration,letter-spacing,word-spacing,line-height,white-space
       Background:
-        background-color,background-image,background-repeat,background-position,
+        background-color,background-image,background-repeat, TODO >> background-position,
         background-attachment
       Dimensions:
         width,height
@@ -111,81 +113,194 @@
         list-style-image,list-style-position,list-style-type,marker-offset
    */
   options.propertyMap = {
-    'background-color':  { type: options.types['color'] },
-    'border-color':      { type: options.types['color'] },
-    'color':             { type: options.types['color'], opts: {label: "Text Color"} },
-    'font-family':       { type: options.types['_select'], opts: {label: "Font", options: options.FONTS} },
-    'font-size':         { type: options.types['dimension'] },
-    'height':            { type: options.types['dimension'] },
-    'margin':            { type: options.types['dimension'] },
-    'padding':           { type: options.types['dimension'] },
-    'text':              { type: options.types['text'] },
-    'text-align':        { type: options.types['text-align'] },
-    'vertical-align':    { type: options.types['vertical-align'] },
-    'width':             { type: options.types['dimension'] },
+    'background-color':    { type: options.types['color'] },
+    'background-image':    { type: options.types['_url'] },
+    'background-repeat':   { type: options.types['_select'], opts: {options: ['no-repeat', 'repeat', 'repeat-x', 'repeat-y']} },
+    'border-color':        { type: options.types['color'] },
+    'color':               { type: options.types['color'], opts: {label: "Text Color"} },
+    'font-family':         { type: options.types['_select'], opts: {options: options.FONTS} },
+    'font-size':           { type: options.types['_dimension'] },
+    'font-style':          { type: options.types['_select'], opts: {options: [ 'normal', 'italic', 'oblique' ] }, },
+    'font-weight':         { type: options.types['_select'], opts: {options: [ 'lighter', 'normal', 'bold' ] }, },
+    'height':              { type: options.types['_dimension'] },
+    'letter-spacing':      { type: options.types['_dimension'], opts: {units: ['px']} },
+    'line-height':         { type: options.types['_dimension'] },
+    'margin':              { type: options.types['_dimension'] },
+    'padding':             { type: options.types['_dimension'] },
+    'text':                { type: options.types['text'] },
+    'text-align':          { type: options.types['_radio'], opts: {options: ['left', 'center', 'right']} },
+    'text-transform':      { type: options.types['_select'], opts: {options: [ 'none', 'uppercase', 'bold' ] }, },
+    'text-decoration':     { type: options.types['_select'], opts: {options: [ 'none', 'underline', 'line-through', 'overline' ] }, },
+    'vertical-align':      { type: options.types['vertical-align'] },
+    'width':               { type: options.types['_dimension'] },
+    'white-space':         { type: options.types['_select'], opts: {options: ['normal', 'nowrap', 'pre']} },
+    'word-spacing':        { type: options.types['_dimension'], opts: {units: ['px']} },
   };
-  
-  /* private class */
+
+  /* private classes */
+  var helpers = {
+    parseLabel: function(property, opts) {
+      if (opts && opts.label) {
+        // Use label override if provided
+        return opts.label;
+      } else {
+        // if the opt.label is not set, generate a label based on @property
+        return property.replace('-', ' ').trim().replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+      }
+    },
+    invertColor: function(str) {
+      if (str.match(/^#[0-9a-fA-F]{6}$/) != null) {
+        return helpers.invertColorHex(str);
+      } else if (str.match(/^#[0-9a-fA-F]{3}$/) != null) {
+        fullHex = str.replace(/[0-9a-fA-F]/g, function(x){return ''+x+x;});
+        return helpers.invertColorHex(fullHex);
+      } else if (str.match(/^rgb\(([0-9]{1,3}, *){2}[0-9]{1,3}\)/) != null) {
+        var rgbBits = str.split('(')[1].split(')')[0].split(',');
+        var hexBits = rgbBits.map(function(x) {
+          x = parseInt(x).toString(16);      //Convert to a base16 string
+          return (x.length==1) ? "0"+x : x;  //Add zero if we get only one character
+        });
+        var hex = '#'+hexBits.join('');
+        return helpers.invertColorHex(hex);
+      } else if (str.match(/^rgba\(([0-9]{1,3}, *){3}[0-9]{1,3}\)/) != null) {
+        return helpers.invertColor(str.replace('rgba', 'rgb').replace(/, *[0-9.]+\)$/, ')'));
+      }
+    },
+    invertColorHex: function(hex) {
+      var color = hex;
+      color = color.substring(1);           // remove #
+      color = parseInt(color, 16);          // convert to integer
+      color = 0xFFFFFF ^ color;             // invert three bytes
+      color = color.toString(16);           // convert to hex
+      color = ("000000" + color).slice(-6); // pad with leading zeros
+      color = "#" + color;                  // prepend #
+      return color;
+    }
+  };
+
+  var elBuild = {
+    fieldset: function(legend, hidden) {
+      var fieldset = $('<fideldset style="position:relative;display:block;">');
+      if (legend) {
+        fieldset.append($('<legend>'+legend+'</legend>'));
+        var legend = fieldset.find('legend');
+        legend.addClass(hidden ? 'elmod-closed' : 'elmod-open');
+        legend.click(function() {
+          var $this = $(this),
+              target = $this.parent().find('.row');
+          // Prevent rapid clicking bugs
+          if ($this.moving) return;
+          $this.moving = true;
+          // Toggle indicator +/-
+          var isHidden = target.is(':visible');
+          $this.toggleClass('elmod-open', !isHidden);
+          $this.toggleClass('elmod-closed', isHidden);
+          // Animate slide
+          target.slideToggle(300, function() {$this.moving=false;});
+        });
+      }
+      return fieldset;
+    },
+    row: function($this, fields) {
+      var row = $('<div class="row"></div>'),
+          colClass = options.COLCLASS + Math.floor(options.COLCOUNT / fields.length);
+      for (var i = 0; i < fields.length; i += 1) {
+        row.append( $('<div class="'+colClass+'">').append(elBuild.field($this, fields[i])) );
+      }
+      return row;
+    },
+    field: function($this, property) {
+      if (typeof property === "string") {
+        var mapped = options.propertyMap[property];
+        if (mapped) {
+          var tmpl = $('<div class="elmod-field">');
+          var opts = $.extend({}, mapped.opts, { label: helpers.parseLabel(property, mapped.opts) });
+          // Get the label for @property
+          tmpl.append($('<label>'+opts.label+'</label>'));
+          // Get the form elements for @property
+          tmpl.append(mapped.type($this, property, opts));
+          return tmpl;
+        } else {
+          return property + " has not been setup in ElMod.js.";
+        }
+      } else if (property instanceof jQuery) {
+        return property;
+      } else {
+        return "error: "+property+" is not the correct format for ElMod.js";
+      }
+    },
+    dimension: function(units) {
+      var tmpl = $('<div>');
+      tmpl.append($('<input type="number">'));
+      if (units.length > 1) {
+        tmpl.append(elBuild.select(units));
+      } else {
+        tmpl.append('<span class="elmod-static-unit">'+units+'</span>');
+      }
+      return tmpl;
+    },
+    select: function(options) {
+      var tmpl = $('<select>');
+      for (var i = 0; i < options.length; i += 1) {
+        tmpl.append('<option value="'+options[i]+'">'+options[i]+'</option>');
+      }
+      return tmpl;
+    },
+    radio: function(property, options) {
+      var tmpl = $('<div>');
+      for (var i = 0; i < options.length; i +=1 ) {
+        var id = options[i];
+        var str = '<input type="radio" name="'+property+'" id="'+id+'">';
+        str += '<label for="'+id+'">'+id.replace(/^[a-z]/, function(s){return s.toUpperCase();})+'</label>';
+        tmpl.append($(str));
+      }
+      return tmpl;
+    }
+  };
+
   ElMod.prototype = {
     constructor: ElMod,
       /* Create a form in jQuery, and store it on the elemnt
        *
-       * *arr* expected structure:
+       * *fieldsets* expected structure:
        *  [
-       *    { title: "Image", fields: $('<div>...</div>') },
-       *    { title: "Align", fields: ["text-align", "vertical-align"],
-       *    { title: "Advanced", fields: ["transform", "box-shadow", "border-radius"], hidden: true }
+       *    { legend: "Image", fields: $('<div>...</div>') },
+       *    { legend: "Align", fields: ["text-align", "vertical-align"],
+       *    { legend: "Advanced", fields: ["transform", "box-shadow", "border-radius"], hidden: true }
+       *    { legend: "Multirow", fields: [["transform", "box-shadow"], ["border-radius"]] }
        *  ]
        *
        * Each element of parent array becomes a fieldset
-       * [i].title becomes the title of the fieldset
-       * [i].fields become the list of fields in the fiedlset
+       * [i].legend becomes the legend of the fieldset
+       * [i].fields become the list of fields in the fiedlset. if fields is a multi-dimensional array, make multiple rows
        * [i].hidden collapses the fieldset, the user can click to expand it.
        */
-    initForm: function(arr, $this) {
+    initForm: function(fieldsets, $this) {
       this.form = $('<form>');
-      var colClass = options.COLCLASS + Math.floor(options.COLCOUNT / arr.length);
-      for (var i = 0; i < arr.length; i += 1) {
-        var cur = arr[i],
-            fieldset = $('<fideldset style="position:relative;display:block;">').append($('<legend>'+cur.title+'</legend>')),
-            row = $('<div class="row"></div>');
-        // Setup expanding functionality
-        if (cur.hidden) {
-          fieldset.append('<span class="indicator" style="position:absolute;top:-0;right:1em;">+</span>');
-          fieldset.click(function() {
-            var $this = $(this),
-                target = $this.find('.row'),
-                indicator = $this.find('.indicator');
-            // Prevent rapid clicking bugs
-            if ($this.moving) return;
-            $this.moving = true;
-            // Toggle indicator +/-
-            indicator.text( target.is(':visible') ? '+' : '-' );
-            // Animate slide
-            target.slideToggle(300, function() {$this.moving=false;});
-          });
-          row.hide();
+      for (var i = 0; i < fieldsets.length; i += 1) {
+        var cur = fieldsets[i]
+            curFields = cur.fields,
+            fieldset = elBuild.fieldset(cur.legend, cur.hidden);
+        // Is String
+        if (typeof curFields[0] === "string") {
+          fieldset.append(elBuild.row($this, curFields));
         }
-        for (var j = 0; j < cur.fields.length; j += 1) {
-          var property = cur.fields[j];
-          if (typeof property === "string") {
-            var mapped = options.propertyMap[property];
-            if (mapped) {
-              var optOverrides = {};
-              if (!mapped.opts || !mapped.opts.label) {
-                // if the opt.label is not set, generate a label based on @property
-                optOverrides.label = property.replace('-', ' ').trim().replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
-              }
-              var opts = $.extend({}, mapped.opts, optOverrides);
-              // Get the form elements for @property
-              property = mapped.type($this, property, opts);
-            } else {
-              property = property + " has not been setup in ElMod.js.";
-            }
+        // Is Array
+        else if (curFields[0] instanceof Array) {
+          for (var j = 0; j < curFields.length; j += 1) {
+            fieldset.append(elBuild.row($this, curFields[j]));
           }
-          row.append( $('<div class="'+colClass+'">').append(property) );
         }
-        this.form.append(fieldset.append(row));
+        // Is jQuery object
+        else if (curFields instanceof jQuery) {
+          fieldset.append(curFields);
+        }
+
+        // If it's supposed to be collapsed, colapse it
+        if (cur.hidden) {
+          fieldset.find('.row').hide();
+        }
+        this.form.append(fieldset);
       }
     },
     getForm: function() {return this.form;},
@@ -203,11 +318,13 @@
   }
   // init form
   $.fn.elModInit = function(structure) {
-    var $this = $(this),
-        mod = new ElMod;
-    mod.initForm(structure, $this);
-    $this.data('ElMod', mod);
-  }
+    $(this).each(function() {
+      var $this = $(this),
+          mod = new ElMod;
+      mod.initForm(structure, $this);
+      $this.data('ElMod', mod);
+    });
+  };
   // get form
   $.fn.elModGet = function() {
     var $this = $(this);
